1c67af418687b02da6643d2602f786b63b0d0ac1
gumelar <gumelar.wicaksono6@gmail.com>
2023-03-31 22:56:22 +0700

    update image ecommerce-w

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-// export const api = 'https://tan-cautious-sea-urchin.cyclic.app';
-export const api = 'http://localhost:5000';
+export const api = 'https://tan-cautious-sea-urchin.cyclic.app';
+// export const api = 'http://localhost:5000';
